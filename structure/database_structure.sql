ff7a265423fb63f7e1e88caaa5cbfe2b932f43fb
SoleBustosAguiar <163673598+SoleBustosAguiar@users.noreply.github.com>
2024-05-13 12:58:49 -0300

    Update database_structure.sql

--- structure/database_structure.sql
+++ structure/database_structure.sql
@@ -3,7 +3,7 @@ DROP DATABASE IF EXISTS cine ;
 CREATE DATABASE cine DEFAULT CHARACTER SET utf8 ; 
 USE cine;
 
-CREATE TABLE Actor (
+CREATE TABLE actor (
   id_actor INT NOT NULL,
   animado TINYINT(1) NULL,
   apellido VARCHAR(200) NULL,
@@ -11,13 +11,13 @@ CREATE TABLE Actor (
   CONSTRAINT PK_ACTOR PRIMARY KEY (id_actor)
 );
 
-CREATE TABLE Calificacion (
+CREATE TABLE calificacion (
   id_calificacion INT NOT NULL,
   nombre VARCHAR(200) NULL,
   CONSTRAINT PK_CALIFICACION PRIMARY KEY (id_calificacion)
 );
 
-CREATE TABLE Cine (
+CREATE TABLE cine (
   id_cine INT NOT NULL,
   direccion VARCHAR(200) NULL,
   fecha_inauguracion DATETIME NULL,
@@ -26,7 +26,7 @@ CREATE TABLE Cine (
   CONSTRAINT PK_CINE PRIMARY KEY (id_cine)
 );
 
-CREATE TABLE Entrada (
+CREATE TABLE entrada (
   id_entrada INT NOT NULL,
   fecha_hora_funcion DATETIME NULL,
   fecha_hora_venta DATETIME NULL,
@@ -36,7 +36,7 @@ CREATE TABLE Entrada (
   CONSTRAINT PK_ENTRADA PRIMARY KEY (id_entrada)
 );
 
-CREATE TABLE Funcion (
+CREATE TABLE funcion (
   numero INT(11) NOT NULL,
   dia_semana VARCHAR(45) NULL DEFAULT NULL,
   duracion VARCHAR(45) NULL DEFAULT NULL,
@@ -47,13 +47,13 @@ CREATE TABLE Funcion (
   CONSTRAINT PK_FUNCION PRIMARY KEY (numero)
 );
 
-CREATE TABLE Genero (
+CREATE TABLE genero (
   id_genero INT(11) NOT NULL,
   nombre VARCHAR(45) NULL DEFAULT NULL,
   CONSTRAINT PK_GENERO PRIMARY KEY (id_genero)
 );
 
-CREATE TABLE HorarioFuncion (
+CREATE TABLE horariofuncion (
   id_horario_funcion INT(11) NOT NULL,
   duracion_intervalo INT(11) NULL DEFAULT NULL,
   duracion_publicidad INT(11) NULL DEFAULT NULL,
@@ -64,14 +64,14 @@ CREATE TABLE HorarioFuncion (
   CONSTRAINT PK_HORARIOFUNCION    PRIMARY KEY (id_horario_funcion)
 );
 
-CREATE TABLE PaisDeOrigen (
+CREATE TABLE paisdeorigen (
   id_pais_de_origen INT NOT NULL,
   idioma VARCHAR(45) NULL,
   nombre VARCHAR(45) NULL,
   CONSTRAINT PK_PAISDEORIGEN PRIMARY KEY (id_pais_de_origen)
 );
 
-CREATE TABLE Pelicula (
+CREATE TABLE pelicula (
   id_pelicula INT NOT NULL,
   anio_estreno INT NULL,
   disponible TINYINT(1) NOT NULL,
@@ -85,14 +85,14 @@ CREATE TABLE Pelicula (
   CONSTRAINT PK_PELICULA PRIMARY KEY (id_pelicula)
 );
 
-CREATE TABLE Personaje (
+CREATE TABLE personaje (
   id_personaje INT NOT NULL,
   nombreEnPelicula VARCHAR(40) NOT NULL,
   id_actor INT NOT NULL,
   CONSTRAINT PK_PERSONAJE PRIMARY KEY (id_personaje)
 );
 
-CREATE TABLE Programacion (
+CREATE TABLE programacion (
   id_programacion INT(11) NOT NULL,
   fecha_fin DATETIME NULL DEFAULT NULL,
   fecha_hora_creada DATETIME NULL DEFAULT NULL,
@@ -101,20 +101,20 @@ CREATE TABLE Programacion (
   CONSTRAINT PK_PROGRAMACION PRIMARY KEY (id_programacion)
 );
 
-CREATE TABLE Reparto (
+CREATE TABLE reparto (
   id_personaje INT(11) NOT NULL,
   id_pelicula INT(11) NOT NULL,
   CONSTRAINT PK_REPARTO PRIMARY KEY (id_personaje, id_pelicula)
 );
 
-CREATE TABLE Rol (
+CREATE TABLE rol (
   id_rol INT NOT NULL,
   nombre VARCHAR(100) NULL,
   id_personaje INT NOT NULL,
   CONSTRAINT PK_ROL PRIMARY KEY (id_rol)
 );
 
-CREATE TABLE Sala (
+CREATE TABLE sala (
   id_sala INT(11) NOT NULL,
   capacidad INT(11) NULL DEFAULT NULL,
   numero INT(11) NULL DEFAULT NULL,
@@ -122,7 +122,7 @@ CREATE TABLE Sala (
   CONSTRAINT PK_SALA PRIMARY KEY (id_sala)
 );
 
-CREATE TABLE Sexo (
+CREATE TABLE sexo (
   id_sexo INT(11) NOT NULL,
   nombre VARCHAR(100) NULL DEFAULT NULL,
   id_actor INT(11) NOT NULL,
